8693729a7050f5da8f6be0677495584f49941292
alinaik-gh <bssevu@gmail.com>
2021-09-08 20:01:19 +0500

    v2

---
+++ dist/todoItem.js
@@ -0,0 +1,30 @@
+"use strict";
+// export class TodoItem {
+//         public id: number;
+//         public task: string;
+//         public complete: boolean = false;
+//         public constructor(id: number, task: string, complete: boolean = false) {
+//             this.id = id;
+//             this.task = task;
+//             this.complete = complete;
+//         }
+//         public printDetails() : void {
+//             console.log(`${this.id}\t${this.task} ${this.complete
+//                 ? "\t(complete)": ""}`);
+//         }
+//     }
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.TodoItem = void 0;
+class TodoItem {
+    constructor(id, task, complete = false) {
+        this.id = id;
+        this.task = task;
+        this.complete = complete;
+        // no statements required
+    }
+    printDetails() {
+        console.log(`${this.id}\t${this.task} ${this.complete
+            ? "\t(complete)" : ""}`);
+    }
+}
+exports.TodoItem = TodoItem;
